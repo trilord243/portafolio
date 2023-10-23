6788c67ada26c4cb086e18908a2fe9e4f1965730
Felipe Escalona <escalonaf12@gmail.com>
2023-10-22 22:44:51 -0400

    responsive design

--- src/components/AchviementsSection.jsx
+++ src/components/AchviementsSection.jsx
@@ -27,39 +27,32 @@ const achievementList = [
 export const AchviementsSection = () => {
     return (
         <div className='py-8 px-4 xl:gap-10 sm:py-16 xl:px-16' >
-            <div className='border-[#33353F] border roundend-md py-8 px-17 flex flex-row items-center justify-between'>
-
-
-
-
+            <div className='border-[#33353F] border rounded-md py-8 px-4 sm:px-6 md:px-8 xl:px-17 flex flex-col sm:flex-row items-center justify-between space-y-6 sm:space-y-0'>
                 {achievementList.map((achievement, index) => {
                     return (
-
-
-                        <div key={index} className='flex flex-col items-center mx-4' >
-                            <h2 className='text-white text-4xl font-bold flex flex-row' >
+                        <div key={index} className='flex flex-col items-center mx-2 sm:mx-4' >
+                            <h2 className='text-white text-3xl sm:text-4xl font-bold flex flex-row items-center'>
                                 {achievement.postfix}
-
-                                <AnimatedNumbers includeComma animateToNumber={parseInt(achievement.value)} locale='en-US' className="text-white text-4xl font-bold" configs={((_, index) => {
-                                    return {
-                                        mass: 1,
-                                        friction: 100,
-                                        tension: 140 * (index + 1),
-
-                                    }
-                                })} /> </h2>
-                            <p className='text-[#ADB7BE] text-base' > {achievement.metric} </p>
+                                <AnimatedNumbers
+                                    includeComma
+                                    animateToNumber={parseInt(achievement.value)}
+                                    locale='en-US'
+                                    className="text-white text-3xl sm:text-4xl font-bold"
+                                    configs={((_, index) => {
+                                        return {
+                                            mass: 1,
+                                            friction: 100,
+                                            tension: 140 * (index + 1),
+                                        }
+                                    })}
+                                />
+                            </h2>
+                            <p className='text-[#ADB7BE] text-base sm:text-lg' > {achievement.metric} </p>
                         </div>
                     )
-                }
-
-
-
-                )
-                }
+                })}
             </div>
-
-
         </div>
     )
 }
+
